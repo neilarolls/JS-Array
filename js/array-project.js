7b11271b08e062ec06f01f6f0ae687705a582776
neilarolls <neilarolls@gmail.com>
2026-06-07 15:37:38 +0100

    Email section done.
    Moved on to image selector now. Getting effects buttons styled.

--- js/array-project.js
+++ js/array-project.js
@@ -48,13 +48,14 @@ $(document).ready( function() {
 // -------------------------------------------------------------------------------------
 
 
+    const firstTitleText = document.getElementById("header-title-wrapper");
     const firstHeaderTitle = document.getElementById("header-title-top");
     const firstHeaderIntro = document.getElementById("header-intro-top");
     const firstAddressTop = document.getElementById("address-manager-top");
     const firstImageMngrTop = document.getElementById("image-manager-top");
     const firstGalleryTop = document.getElementById("gallery-top");
     const firstCurrentWidth = window.innerWidth;
-    const maxBannerHeight = 300;
+    // const maxBannerHeight = 300;
     let firstCalculatedHeight = Math.ceil(firstCurrentWidth * 0.5625);
 
     // if (firstCalculatedHeight > maxBannerHeight) {
@@ -64,8 +65,12 @@ $(document).ready( function() {
     const firstIntroHeight = firstHeaderIntro.clientHeight;
     const firstAddressHeight = firstAddressTop.clientHeight;
     const firstImgMngrHeight = firstImageMngrTop.clientHeight;
+    const firstNewFontSize = (firstCurrentWidth / 768) + 2;
     let currentBannerImageSeed = Math.random().toString(36).substring(2, 9);
     let selectedAddress = "";
+
+
+    $(firstTitleText).css({"font-size":`${firstNewFontSize}rem`});
 
     $(firstHeaderIntro).css({"position":"absolute","left":"0","top":`${firstCalculatedHeight}px`});
 
@@ -83,6 +88,7 @@ $(document).ready( function() {
 
     function changeHeader() {
 
+        const titleText = document.getElementById("header-title-wrapper");
         const headerTitle = document.getElementById("header-title-top");
         const headerIntro = document.getElementById("header-intro-top");
         const addressTop = document.getElementById("address-manager-top");
@@ -99,6 +105,10 @@ $(document).ready( function() {
         let introHeight = headerIntro.clientHeight;
         let addressHeight = addressTop.clientHeight;
         let imgMngrHeight = imageMngrTop.clientHeight;
+
+        const newFontSize = (currentWidth / 768) + 2;
+
+        $(titleText).css({"font-size":`${newFontSize}rem`});
 
         const imageSeed = Math.random().toString(36).substring(2, 9);
 
@@ -178,7 +188,7 @@ $(document).ready( function() {
 
     function isUnique(idString) {
 
-        let test = document.getElementById(idString);
+        let test = document.getElementById(`btn${idString}`);
 
         if (test) {
             return false;
@@ -203,6 +213,7 @@ $(document).ready( function() {
             let addressText = "";
             let imageCount = 0;
             let buttonText = "";
+            let newButton = "";
 
             if (update === "new") {
 
@@ -217,32 +228,112 @@ $(document).ready( function() {
                     imageCount = Object.keys(imageLinks[i].images).length;
 
                     // Builds the string for the button text.
-                    buttonText = addressText + " (" + `${imageCount})`;
+                    buttonText = addressText + " <br> " + `${imageCount} images assigned`;
 
                     // Inserts the html at the end of the div. The div is given a unique id containing the email address. The button gets only the email address as an id.
                     // This means an event handler on the button returns the email address string which is the key for the array object it represents. selectedAddress can
-                    // be set directly to this value after making the visual change showing button selection.
-                    addressList.insertAdjacentHTML('beforeend', `<div class="address-manager-email-display" id="div${addressText}"><button id="${addressText}" type="button">${buttonText}</button></div>`);
+                    // be set to this value after making the visual change showing button selection.
+                    addressList.insertAdjacentHTML('beforeend', `<div class="address-manager-email-display" id="div${addressText}"><button id="btn${addressText}" class="btn-not-selected" type="button">${buttonText}</button></div>`);
 
-                    // console.log(addressText);
-                    // console.log(imageCount);
-                    // console.log(buttonText);
+                    // Gets the button just created.
+                    newButton = document.getElementById(`btn${addressText}`);
+
+                    // Add event listener on new button.
+                    newButton.addEventListener("click", (e) => {
+
+                        // Suppress any default behaviour.
+                        e.preventDefault();
+
+                        // Get id of button that was clicked, which will be the address the button represents.
+                        let newAddress = e.target.id.substring(3);
+
+                        // Get the newly selected button.
+                        const newlySelected = document.getElementById(`btn${newAddress}`);
+
+                        // Get the previously selected button.
+                        const previouslySelected = (selectedAddress)?document.getElementById(`btn${selectedAddress}`):"";
+
+                        // If there was a button previously selected and you haven't clicked on that same one,
+                        // remove `btn-selected` class and add `btn-not-selected` class to previous button.
+                        // Then do the reverse for the new button. Finally set selectedAddress to the new
+                        // address.
+                        if (previouslySelected && (selectedAddress != newAddress)) {
+
+                            previouslySelected.classList.remove("btn-selected");
+                            previouslySelected.classList.add("btn-not-selected");
+
+                            newlySelected.classList.remove("btn-not-selected");
+                            newlySelected.classList.add("btn-selected");
+
+                            selectedAddress = newAddress;
+
+                        } else {
+
+                            // At first no buttons are selected
+                            newlySelected.classList.remove("btn-not-selected");
+                            newlySelected.classList.add("btn-selected");
+
+                            selectedAddress = newAddress;
+                        }
+                    });
 
                 }
 
             } else if (update === "addaddress") {
 
-                    // Creates a button for a newly added address...
+                // Creates a button for a newly added address...
 
 
-                    // Gets the last address in the array and stores it.
-                    addressText = imageLinks[arrayLength - 1]["address"];
+                // Gets the last address in the array and stores it.
+                addressText = imageLinks[arrayLength - 1]["address"];
 
-                    // Builds the string for the button text.
-                    buttonText = addressText + " (0)";
+                // Builds the string for the button text.
+                buttonText = addressText + " <br> 0 images assigned";
 
-                    // Inserts the html at the end of the div. (see above also)
-                    addressList.insertAdjacentHTML('beforeend', `<div class="address-manager-email-display" id="div${addressText}"><button id="${addressText}" type="button">${buttonText}</button></div>`);
+                // Inserts the html at the end of the div. (see above also)
+                addressList.insertAdjacentHTML('beforeend', `<div class="address-manager-email-display" id="div${addressText}"><button id="btn${addressText}" class="btn-not-selected" type="button">${buttonText}</button></div>`);
+
+                // Gets the button just created.
+                newButton = document.getElementById(`btn${addressText}`);
+
+                // Add event listener on new button.
+                newButton.addEventListener("click", (e) => {
+
+                    // Suppress any default behaviour.
+                    e.preventDefault();
+
+                    // Get id of button that was clicked, which will be the address the button represents.
+                    let newAddress = e.target.id.substring(3);
+
+                    // Get the newly selected button.
+                    const newlySelected = document.getElementById(`btn${newAddress}`);
+
+                    // Get the previously selected button.
+                    const previouslySelected = (selectedAddress)?document.getElementById(`btn${selectedAddress}`):"";
+
+                    // If there was a button previously selected and you haven't clicked on that same one,
+                    // remove `btn-selected` class and add `btn-not-selected` class to previous button.
+                    // Then do the reverse for the new button. Finally set selectedAddress to the new
+                    // address.
+                    if (previouslySelected && (selectedAddress != newAddress)) {
+
+                        previouslySelected.classList.remove("btn-selected");
+                        previouslySelected.classList.add("btn-not-selected");
+
+                        newlySelected.classList.remove("btn-not-selected");
+                        newlySelected.classList.add("btn-selected");
+
+                        selectedAddress = newAddress;
+
+                    } else {
+
+                        // At first no buttons are selected
+                        newlySelected.classList.remove("btn-not-selected");
+                        newlySelected.classList.add("btn-selected");
+
+                        selectedAddress = newAddress;
+                    }
+                });
 
             }
         }
